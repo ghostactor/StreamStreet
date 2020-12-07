11ef0bb5c8ef4b14ef3ceab54a30473eacb4cf0d
ghostofpokemon <74148990+ghostofpokemon@users.noreply.github.com>
2020-12-06 16:58:07 -0800

    Update r.py

--- r.py
+++ r.py
@@ -16,12 +16,12 @@ epd = epd2in13_V2.EPD()
 epd.init(epd.FULL_UPDATE)
 epd.Clear(0xFF)
 
-font15 = ImageFont.truetype(os.path.join(picdir, 'GnuUnifont.ttf'), 15)
+font14 = ImageFont.truetype(os.path.join(picdir, 'GnuUnifont.ttf'), 14)
 
 def printToDisplay(string):
     image = Image.new('1', (epd.height, epd.width), 255)
     draw = ImageDraw.Draw(image)
-    draw.text((0, 10), string, font = font15, fill = 0)
+    draw.text((0, 10), string, font = font14, fill = 0)
     epd.display(epd.getbuffer(image))
 
 
